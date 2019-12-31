26f7cffd6960e74a2faf3231b9eda3b716573460
pierry01 <pierrybm@gmail.com>
2019-12-31 16:54:08 +0000

    first commit

---
+++ app/models/contact.rb
@@ -0,0 +1,8 @@
+class Contact < ApplicationRecord
+  belongs_to :kind
+  has_many :phones
+  has_one :address
+  
+  accepts_nested_attributes_for :phones, allow_destroy: true
+  accepts_nested_attributes_for :address, update_only: true
+end
---
+++ app/serializers/contact_serializer.rb
@@ -0,0 +1,20 @@
+class ContactSerializer < ActiveModel::Serializer
+  attributes :id, :name, :email, :birthdate
+
+  belongs_to :kind do
+    link(:related) { kind_url(object.kind.id) }
+  end
+
+  has_many :phones
+  has_one :address
+
+  meta do
+    { author: 'Jean Mendonça' }
+  end
+
+  def attributes(*args)
+    hash = super(*args)
+    hash[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+    hash
+  end
+end
